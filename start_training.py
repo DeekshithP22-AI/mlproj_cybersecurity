3c29c21a7b6f3f55e8414ad45bd3eed60c37b5ec
DeekshithP22-AI <deekshithwork22@gmail.com>
2024-11-29 16:40:50 +0530

    created training master file
    created training master file

--- start_training.py
+++ start_training.py
@@ -4,6 +4,7 @@ import sys
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logger.logger import logging
 
+
 from networksecurity.pipeline.training_pipeline import TrainingPipeline
 
 
